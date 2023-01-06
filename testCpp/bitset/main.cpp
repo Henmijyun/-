66c31b8b07feb63c34191c1ebe726c2b54862e1e
CJX <1106671528@qq.com>
2023-01-06 23:02:31 +0800

    1/6 更新BloomFilter

--- testCpp/bitset/main.cpp
+++ testCpp/bitset/main.cpp
@@ -1,10 +1,14 @@
 #include "myBitset.h"
+#include "BloomFilter.h"
 
 int main()
 {
     skk::test_bitset1();
     skk::test_bitset3();
 
+    cout << endl;
+    TestBloomFilter1(); 
+    TestBloomFilter2(); 
     return 0;
 }
 
